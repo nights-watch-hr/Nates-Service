f03dae02f67d8feeda97fdd7142ff9ad7cab1d5e
Nate <nathanaelmullins@gmail.com>
2019-04-05 20:41:22 -0700

    [prog] keyboard shortcuts modal

--- client/src/components/mediaPlayerSubs/playerButtonsSubs/keyboardShortcuts.jsx
+++ client/src/components/mediaPlayerSubs/playerButtonsSubs/keyboardShortcuts.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import KeyboardShortcutsModal from '../../queueSubs/keyboardShortcutsModal';
 import KeyboardIcon from '../../../../icons/keyboardIcon';
 import style from '../../../../styles/keyboardShortcuts';
 
-const keyboardShortcuts = props => {
-  return (
-    <div className={style.keyboardShortcutContainer}>
-      <a id="placeholder for Keyboard Shortcuts Modal" href="">
-        <KeyboardIcon />
-      </a>
-    </div>
-  );
-};
+class KeyboardShortcuts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: true
+    };
+    this.openModal = this.openModal.bind(this);
+  }
 
-export default keyboardShortcuts;
+  openModal() {
+    let modalOpen = !this.state.modalOpen;
+    this.setState({ modalOpen });
+  }
+
+  render() {
+    return (
+      <div className={style.keyboardShortcutContainer}>
+        <a onClick={this.openModal}>
+          <KeyboardIcon />
+        </a>
+        {this.state.modalOpen && (
+          <KeyboardShortcutsModal
+            className={style.modal}
+            closeModal={this.openModal}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default KeyboardShortcuts;
