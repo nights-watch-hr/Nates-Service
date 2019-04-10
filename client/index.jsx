308eb2ca7f613349d5b1bcfd973939b32f4685a7
Nate <nathanaelmullins@gmail.com>
2019-04-10 15:51:25 -0700

    [prog] global export for proxy

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import MediaPlayer from './src/components/mediaPlayer';
 
-render(<MediaPlayer />, document.getElementById('app'));
+render(<MediaPlayer />, document.getElementById('mediaPlayer'));
+
+window.MediaPlayer = MediaPlayer;
