790cbcb76d41b61734d0c2503f76e441105223e5
Nate <nathanaelmullins@gmail.com>
2019-04-13 18:14:15 -0700

    [style] waveform placement

--- client/src/components/mediaPlayerSubs/waveform.jsx
+++ client/src/components/mediaPlayerSubs/waveform.jsx
@@ -5,8 +5,8 @@ const Waveform = props => (
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
     width="41.7vw"
-    height="73"
-    viewBox="0 0 1194 190"
+    height="70"
+    viewBox="0 -3 1194 190"
     preserveAspectRatio="none"
   >
     <image href={props.svg} />
